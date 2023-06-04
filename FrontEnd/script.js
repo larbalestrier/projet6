51e8c4ad73487662a3a1b0014c5495ae141b178d
Quentin l'arbalestrier <quentin.larbalestrier@gmail.com>
2023-06-04 19:33:49 +0200

    étape 1.2

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,11 +1,15 @@
-const gallery = document.querySelector('#gallery');   
-    
+// Sélectionner l'élément DOM qui contiendra la galerie
 
+const gallery = document.querySelector('#gallery');   
+const divCategories = document.querySelector("#filtres");
+    
+// appel de l'api et création des projets //
 fetch("http://localhost:5678/api/works")
   .then(response => response.json())
   .then(data => {
     data.forEach(project => {
       const projectElement = document.createElement('div');
+      projectElement.setAttribute ("categorieId", project.category.id)
       const imgElement = document.createElement('img');
       const titleElement = document.createElement('h3');
       imgElement.src = `${project.imageUrl}`;
@@ -13,6 +17,60 @@ fetch("http://localhost:5678/api/works")
       projectElement.appendChild(imgElement);
       projectElement.appendChild(titleElement);
       gallery.appendChild(projectElement);
+  
     });
+    console.log(data)
   })
   .catch(error => console.error(error));
+
+// création boutton tous //
+const buttonTous = document.querySelector('#filtres');
+const buttonT = document.createElement('button');
+buttonT.innerText = 'Tous';
+buttonT.setAttribute('class', 'button buttonSelect' );
+buttonTous.appendChild(buttonT);
+buttonT.addEventListener('click',function() {btnTous();colorButton(buttonT)} )
+
+// // 
+fetch("http://localhost:5678/api/categories")
+  .then(response => response.json())
+  .then(dataButton =>{    
+  dataButton.forEach((categorie)=> {
+    const btnCategorie =document.createElement("button");
+    btnCategorie.innerText = categorie.name ;
+    btnCategorie.setAttribute("class", "button");
+    btnCategorie.setAttribute("categorieId", categorie.id);
+    console.log(btnCategorie)
+    divCategories.appendChild(btnCategorie);
+    btnCategorie.addEventListener('click', function(){filtreCategories(categorie.id);colorButton(this)} )
+    })
+})
+
+
+function filtreCategories (id){
+  const projectGallery =  gallery.querySelectorAll("div")
+  projectGallery.forEach(element => {
+    if (element.getAttribute("categorieId") != id) {
+      element.style.display = "none"
+    } else {
+      element.style.display = "unset"
+    }
+  });
+}
+
+
+function btnTous () {
+  const projectGallery =  gallery.querySelectorAll("div")
+  projectGallery.forEach(element => {
+      element.style.display = "unset"
+    }
+  );
+}
+
+
+function colorButton (btn) {
+  const btnSelect = document.querySelector(".buttonSelect")
+  btnSelect.classList.remove("buttonSelect")
+  btn.classList.add("buttonSelect")
+
+}
